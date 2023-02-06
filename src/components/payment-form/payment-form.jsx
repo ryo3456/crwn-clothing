a2bf56858aa1362d509d3d8020df682a0e2a1782
ryo3456 <hochunyin1021@gmail.com>
2023-02-06 17:33:01 +0800

    fixing test

--- src/components/payment-form/payment-form.jsx
+++ src/components/payment-form/payment-form.jsx
@@ -28,6 +28,7 @@ const PaymentForm = () => {
         }).then((res) => {
             return res.json();
         });
+        console.log(response);
         const clientSecret = response.paymentIntent.client_secret;
         const paymentResult = await stripe.confirmCardPayment(clientSecret, {
             payment_method: {
